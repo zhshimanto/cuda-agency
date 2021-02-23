e9e77340d0379fa14eb830b88e616afa537ef085
Zakir Hossain <md.zakir5737@gmail.com>
2021-02-23 16:10:47 +0600

    file upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	var mixer = mixitup('.ll');
+	$('#down').addClass('animated bounceInDown');
+	$('#up').addClass('animated bounceInUp');
+	$('#left').addClass('animated bounceInLeft');
+});
